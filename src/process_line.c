01863e2b1b8ca1ed1651c029fed659effef5d4cb
hyuntall <hyunchul1998@naver.com>
2022-12-04 18:31:02 +0900

    processing dquote

--- src/process_line.c
+++ src/process_line.c
@@ -26,42 +26,81 @@ char	*join_line(char *input, char *word)
 	return (str);
 }
 
+char	*parse_envp(char *key)
+{
+	printf("dgdg%s\n", key);
+	return (key);
+}
+/*
+char	*process_dquote(char *input, char *word)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	str = ft_strdup("");
+	while (input[i])
+	{
+		if (input[i] == '$')
+		{
+			str = join_line(str, ft_substr(input, j, i - j));
+			while (input[i] != ' ' || input[i])
+				i++;
+			str = join_line(str, ft_substr(input, j, i - j));
+		}
+	}
+}*/
+
 char	**process_line(char *input)
 {printf("in process line = %s\n",input);
 	char	*str;
 	char	**cmds;
 	int		i;
 	int		j;
-	int		isquote;
+	int		index;
 
 	i = 0;
-	j = 0;
-	isquote = 0;
+	index = 0;
 	str = ft_strdup("");
 	while (input[i])
 	{
+		j = 0;
 		if (input[i] == '\'')
 		{
-			str = join_line(str, ft_substr(input, j, i - j));
+			str = join_line(str, ft_substr(input, index, i - index));
 			i++;
-			j = i;
-			isquote = 1 - isquote;
+			while (input[i + j] != '\'')
+				j++;
+			str = join_line(str, ft_substr(input, i, j));
+			i += j + 1;
+			index = i;
 		}
 		else if (input[i] == '"')
 		{
 			// process dquote
+			str = join_line(str, ft_substr(input, index, i - index));
+			i++;
+			while (input[i + j] != '"')
+				j++;
+			str = join_line(str, ft_substr(input, i, j));
+			i += j + 1;
+			index = i;
 		}
-		else if (input[i] == ' ' && !isquote)
+		else if (input[i] == ' ')
 		{
-			str = join_line(str, ft_substr(input, j, i - j));
+			str = join_line(str, ft_substr(input, index, i - index));
 			str = join_line(str, ft_strdup("'"));
 			i++;
 			j = i;
+			index = i;
 		}
 		else
 			i++;
 	}
-	str = join_line(str, ft_substr(input, j, i - j));
+	printf("result: %s\n", str);
+	str = join_line(str, ft_substr(input, index, i - index));
 	cmds = ft_split(str, '\'');
 	free(str);
 	str = 0;
