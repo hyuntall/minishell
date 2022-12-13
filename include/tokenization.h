2d5ef4dffdf4b4aec1bd451b5ae04137b39d9fa6
hyuntall <hyunchul1998@naver.com>
2022-12-13 16:11:10 +0900

    write comment and add parenthesis in e_token_type

--- include/tokenization.h
+++ include/tokenization.h
@@ -6,7 +6,7 @@
 /*   By: hyuncpar <hyuncpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:41:09 by jiwonhan          #+#    #+#             */
-/*   Updated: 2022/12/13 14:35:39 by hyuncpar         ###   ########.fr       */
+/*   Updated: 2022/12/13 16:10:31 by hyuncpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,23 @@
 
 typedef enum	e_token_type
 {
-	NORM,
-	SPCE,
-	QUOT,
-	DQUT,
-	BSLH,
-	DOLR,
-	PIPE,
-	DPIP,
-	DAND,
-	SEMC,
-	DSEM,
-	RIGT,
-	DRGT,
-	LEFT,
-	DLFT,
-	EROR
+	NORM, // Normal
+	SPCE, // Space
+	QUOT, // Single Quote
+	DQUT, // Double Quote
+	BSLH, // Back Slash
+	DOLR, // Dollar
+	PIPE, // Pipe
+	DPIP, // Double Pipe (Or Operator)
+	DAND, // Double And (And Operator)
+	SEMC, // Semi Colon
+	DSEM, // Double Semi Colon
+	RIGT, // Right (Over Write)
+	DRGT, // Double Right (Append)
+	LEFT, // Left (Stdin)
+	DLFT, // Double Left (Heredoc)
+	PARENTHESIS, // Parenthesis
+	EROR  // Error
 }			t_token_type;
 
 typedef	struct s_token
@@ -56,4 +57,5 @@ t_token_list	analize_line(t_token_list token_list, char *input);
 int				unexpecte_token(t_token_type type, char *str);
 void			free_tokens(t_token_list *token_list);
 void			init_token_list(t_token_list *token_list);
+char			*process_dquote(char *str);
 #endif
