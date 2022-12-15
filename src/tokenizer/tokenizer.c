f8752f7d22c4e3a0fcad65ee72537d3a820a20af
hyuntall <hyunchul1998@naver.com>
2022-12-15 17:17:32 +0900

    add function of tokenize_prnth in tokenize.c

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hanjiwon <hanjiwon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hyuncpar <hyuncpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:42:09 by hyuncpar          #+#    #+#             */
-/*   Updated: 2022/12/14 21:39:29 by hanjiwon         ###   ########.fr       */
+/*   Updated: 2022/12/15 17:08:52 by hyuncpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,31 @@ t_token	*tokenizer(char *input)
 	//word split->remove space
 	int		i;
 	int		index;
-	t_token	*ret;
+	t_token	*token;
 
 	i = 0;
 	index = 0;
-	ret = NULL;
+	token = NULL;
 	while (input && input[i])
 	{
 		if (input [i] == '\'' || input[i] == '"' || input[i] == ' ' \
 		|| input[i] == '\\' || input[i] == '$' || input[i] == '|' \
-		|| input[i] == '&' || input[i] == '>' || input[i] == '<')
+		|| input[i] == '&' || input[i] == '>' || input[i] == '<' \
+		|| input[i] == '(')
 		{
-			i = tokenize_line(&ret, input, index, i);
+			i = tokenize_line(&token, input, index, i);
 			index = i;
 		}
 		else
 			i++;
 		if (i == TOKEN_ERROR)	//TODO minishell status change
 		{
-			free_tokens(ret);
-			return (ret);
+			free_tokens(token);
+			return (token);
 		}
 	}
 	if (i - index)
-		insert_token(&ret, init_token(ft_substr(input, index, i - index), NORM));
+		insert_token(&token, init_token(ft_substr(input, index, i - index), NORM));
 	//TODO remove space
-	return (ret);
+	return (token);
 }
